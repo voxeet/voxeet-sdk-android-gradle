6b526f121a9df9915bf60eef500e87f02050adcf
Kévin Le Perf <codlabtech@gmail.com>
2022-11-24 06:01:00 +0100

    Fix doc generation

--- sample/library/src/main/kotlin/io/dolby/library/Empty.kt
+++ sample/library/src/main/kotlin/io/dolby/library/Empty.kt
@@ -1,5 +1,23 @@
 package io.dolby.library
 
+/**
+ * Some klass
+ */
 class Empty {
     val isValid = true
+
+    /**
+     * Some print method
+     */
+    fun print() {
+        // nothing to do
+        privateMethod()
+    }
+
+    /**
+     * This method is private
+     */
+    private fun privateMethod() {
+        // nothing to do
+    }
 }
