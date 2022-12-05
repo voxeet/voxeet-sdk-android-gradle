cb0e78123d0ed3b7ccc579e7cd549d7206a72c3a
Kevin Le Perf <kevin.leperf@dolby.com>
2022-12-05 19:24:51 +0100

    Add ability to set specific tests tasks when encountering flavors

---
+++ sample/app/src/main/kotlin/io/dolby/sample/Empty.kt
@@ -0,0 +1,23 @@
+package io.dolby.sample
+
+/**
+ * Some klass
+ */
+class Empty {
+    val isValid = true
+
+    /**
+     * Some print method
+     */
+    fun print() {
+        // nothing to do
+        privateMethod()
+    }
+
+    /**
+     * This method is private
+     */
+    private fun privateMethod() {
+        // nothing to do
+    }
+}
